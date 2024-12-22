fb751b63b6c3558060a0624eb0db5b45fc32fe7c
Miguel Galán Lerate <miggaller@alum.us.es>
2024-12-22 08:22:43 +0100

    feat: Create user model Ref: #9, #10

--- user/models.py
+++ user/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+        if not username or not password:
+            raise ValueError('Please, set username and password')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_admin(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(
+            username=username,
+            password=password,
+            **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+    is_admin = models.BooleanField(default=False, null=False)
+    name = models.CharField(max_length=128, null=True)
+
+    # Relacionar las relaciones inversas para evitar los choques
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',
+        blank=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['password', 'username']
+
+    def __str__(self):
+        return self.username
