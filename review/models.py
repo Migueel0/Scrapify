2710196c627e1ecb1b72fa3944ec8af835f27bac
Miguel Galán Lerate <miggaller@alum.us.es>
2025-01-04 18:30:59 +0100

    Feat: Create review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=50,blank=True)
+    rating = models.FloatField(null=False)
+    title = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    image = models.URLField(null=True)
+    date = models.CharField(blank=True, max_length=50)
+    
+    def __str__(self):
+        return self.product.name + ' - ' + self.author_name
--- scrapify/settings.py
+++ scrapify/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     #Scrapify apps
     'user',
     'product',
+    'review',
     
 ]
 
